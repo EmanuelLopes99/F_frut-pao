e5e0887583caff5f64e30c168b1c90fa02f749aa
EmanuelLopes99 <djonnylopes92@gmail.com>
2018-08-02 16:42:57 -0100

    login

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -14,22 +14,20 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="list-group-item active">
         <h3 class="panel-title" style="text-align: center;">Login</h3>
     </div>
-        <div class="panel-body">
-
-  
+    <div class="panel-body">
+        <div style="margin-top: 25px;">
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+            <?= $form->field($model, 'password')->passwordInput() ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+            <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
+            <div class="form-group">
+                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            </div>
             <?php ActiveForm::end(); ?>
-   
-
-</div></div>
+        </div>
+    </div>
+</div>
